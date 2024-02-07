075ed5d2538fed9f3d9cd6f9cfb64c8bf43346cf
vaibhavbais007 <79183639+vaibhavbais007@users.noreply.github.com>
2024-02-07 09:27:55 +0000

    Operations learned

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -23,4 +23,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************************** Operations *******************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // to the power, i.e 2 to the power 3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Vaibhav"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+// console.log(num1, num2, num3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
