ea7523172743c2cfd619d415a0c87baeca63410c
vaibhavbais007 <79183639+vaibhavbais007@users.noreply.github.com>
2024-02-07 09:05:07 +0000

    datatype conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,26 @@
+let score = "33abc";
+
+// console.log(typeof(score));
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+// convering into boolean
+
+let isLoggedIn = "Vaibhav";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
